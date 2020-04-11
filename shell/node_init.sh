22bed1a4929b6f4b294bd5a91a2a12c5ad3a5b19
nibilin <819231957@qq.com>
2020-04-11 16:05:33 +0800

    format

--- shell/node_init.sh
+++ shell/node_init.sh
@@ -1,8 +1,8 @@
-#!/bin/sh 
-wget http://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-x64.tar.gz 
-tar xf node-v8.11.1-linux-x64.tar.gz -C /usr/local/    
+#!/bin/sh
+wget http://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-x64.tar.gz
+tar xf node-v8.11.1-linux-x64.tar.gz -C /usr/local/
 cd /usr/local/
-mv node-v8.11.1-linux-x64/ nodejs   
-ln -s /usr/local/nodejs/bin/node /usr/local/bin 
-ln -s /usr/local/nodejs/bin/npm /usr/local/bin  
-echo -e "\033[32;49;1mEND\033[39;49;0m"!   
+mv node-v8.11.1-linux-x64/ nodejs
+ln -s /usr/local/nodejs/bin/node /usr/local/bin
+ln -s /usr/local/nodejs/bin/npm /usr/local/bin
+echo -e "\033[32;49;1mEND\033[39;49;0m"!
