07f46430a936caf9ec6d3e99062e1a90f881a346
nibilin33 <nibilin33@gmail.com>
2022-08-17 10:54:51 +0800

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -7,10 +7,26 @@ from requests_html import HTMLSession
 
 # english digest: https://mp.weixin.qq.com/s/JdtaFknT4FBYTBIZM4BqAw
 
-def analyseHtml(url):
-    session = HTMLSession()
+rule_map = {
+    'link': 1,
+    'li': 2
+}
+session = HTMLSession()
+SAVE_PATH = '/Users/nibl/Desktop/雅思学习/作文备考/跟着外刊学写作/'
+
+def analyseRule(url='',type=1):
     r = session.get(url)
-    links = r.html.absolute_links
+    if rule_map['link'] == type:
+        return r.html.absolute_links
+    if rule_map['li'] == type:
+        def get_link(it):
+            return it.attrs['data-link']
+        result = map(get_link,r.html.find('li.album__list-item'))
+        return list(result)
+
+
+def analyseHtml(url,type=1):
+    links = analyseRule(url,type)
     # asysession = AsyncHTMLSession()
     for item in links:
         # find class name 'rich_media_title' or id '#activity-name'
@@ -28,14 +44,14 @@ def createPdf(url,filename):
         client = pdfcrowd.HtmlToPdfClient('demo', 'ce544b6ea52a5621fb9d55f8b542d14d')
          # run the conversion and write the result to a file
         print('start save '+ filename)
-        client.convertUrlToFile(url, filename+'.pdf')
+        client.convertUrlToFile(url, SAVE_PATH+filename+'.pdf')
     except pdfcrowd.Error as why:
     # report the error
         sys.stderr.write('Pdfcrowd Error: {}\n'.format(why))
 
 
 if __name__ ==  '__main__':
-    ana_url = 'https://mp.weixin.qq.com/s/JdtaFknT4FBYTBIZM4BqAw'
-    analyseHtml(ana_url)
+    ana_url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI2MDYzNjQ2MA==&action=getalbum&album_id=1747213984925057027&scene=173&from_msgid=2247499658&from_itemidx=1&count=3&nolastread=1&devicetype=iOS15.2&version=18001a2f&lang=zh_CN&nettype=WIFI&ascene=78&fontScale=100&wx_header=3'
+    analyseHtml(ana_url,2)
 
 
