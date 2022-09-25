1d4d6531d85755fda232ee27979651b71980f330
nibilin33 <nibilin33@gmail.com>
2022-09-25 17:27:42 +0800

    Update html_pdf.py

--- html_pdf.py
+++ html_pdf.py
@@ -17,11 +17,11 @@ SAVE_PATH = '/Users/nibl/Desktop/雅思学习/作文备考/跟着外刊学写作
 
 def analyseRule(url='',type=1):
     r = session.get(url)
+    # 滚动加载处理
+    r.html.render(scrolldown=5,sleep=2)
     if rule_map['link'] == type:
         return r.html.absolute_links
     if rule_map['li'] == type:
-        # 滚动加载处理
-        r.html.render(scrolldown=5,sleep=2)
         def get_link(it):
             return it.attrs['data-link']
         result = map(get_link,r.html.find('li.album__list-item'))
@@ -31,6 +31,7 @@ def analyseRule(url='',type=1):
 def analyseHtml(url,type=1):
     links = analyseRule(url,type)
     # asysession = AsyncHTMLSession()
+    print('total length:'+ str(len(links)))
     for item in links:
         # find class name 'rich_media_title' or id '#activity-name'
         item_view = session.get(item)
@@ -44,7 +45,7 @@ def analyseHtml(url,type=1):
 def createPdf(url,filename):
     try:
         # create the API client instance
-        client = pdfcrowd.HtmlToPdfClient('demo', 'ce544b6ea52a5621fb9d55f8b542d14d')
+        client = pdfcrowd.HtmlToPdfClient('nibilin33', 'c924b725c175f0069056faafee7fe78f')
          # run the conversion and write the result to a file
         print('start save '+ filename)
         client.convertUrlToFile(url, SAVE_PATH+filename+'.pdf')
@@ -54,12 +55,13 @@ def createPdf(url,filename):
 
 
 if __name__ ==  '__main__':
-    ana_url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI2MDYzNjQ2MA==&action=getalbum&album_id=1747213984925057027&scene=173&from_msgid=2247499658&from_itemidx=1&count=3&nolastread=1&devicetype=iOS15.2&version=18001a2f&lang=zh_CN&nettype=WIFI&ascene=78&fontScale=100&wx_header=3'
+    # ana_url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI2MDYzNjQ2MA==&action=getalbum&album_id=1747213984925057027&scene=173&from_msgid=2247499658&from_itemidx=1&count=3&nolastread=1&devicetype=iOS15.2&version=18001a2f&lang=zh_CN&nettype=WIFI&ascene=78&fontScale=100&wx_header=3'
     work_list = [
-        'https://mp.weixin.qq.com/mp/homepage?__biz=Mzg5MDA5Mzk0MQ==&hid=5&sn=4e77f0a0c22a165a643fc7e9e1527e29&scene=18&devicetype=iOS15.2&version=18001a2f&lang=zh_CN&nettype=WIFI&ascene=7&session_us=gh_1bc7b3947244&fontScale=100&wx_header=3',
+        # 'https://mp.weixin.qq.com/mp/homepage?__biz=Mzg5MDA5Mzk0MQ==&hid=5&sn=4e77f0a0c22a165a643fc7e9e1527e29&scene=18&devicetype=iOS15.2&version=18001a2f&lang=zh_CN&nettype=WIFI&ascene=7&session_us=gh_1bc7b3947244&fontScale=100&wx_header=3',
         'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI2MDYzNjQ2MA==&action=getalbum&album_id=2467381321611558912&scene=173&from_msgid=2247500859&from_itemidx=1&count=3&nolastread=1#wechat_redirect',
         'http://mp.weixin.qq.com/mp/homepage?__biz=MzI2MDYzNjQ2MA==&hid=5&sn=434d2fc9f419ea35328fdc38403a425e&scene=18#wechat_redirect'
     ]
-    analyseHtml(ana_url,2)
+    for ana_url in work_list:
+        analyseHtml(ana_url,1)
 
 
