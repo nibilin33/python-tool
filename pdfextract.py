e8111ba107f2f580bc24759b7ab515b94487691f
nibilin33 <nibilin33@gmail.com>
2022-10-09 13:31:44 +0800

    Update pdfextract.py

--- pdfextract.py
+++ pdfextract.py
@@ -4,25 +4,46 @@
 import pdfplumber
 import re,os
 
+global_mode = 'dot'
+
 def extract_page(page,tag):
     text = page.extract_text() #提所有文字
-    print(text)
-    createFile('anki.txt',number_string_rule(text),tag)
+    createFile('anki.txt',text,tag)
 
-def createFile(path, result,tag):
+def createFile(path, text,tag):
+    if global_mode == 'dot':
+        result = dot_string_rule(text)
+    else:
+        result = number_string_rule(text)
     mode = 'w'
     if os.path.exists(path):
            mode = 'a'
     f = open(path, mode)    
     for name in result:
-        f.write("*"+name)
-        f.write('\n')
-        f.write('**'+tag)
-        f.write('\n\n')
+        if global_mode == 'dot':
+            format_dot(name,f)
+        else:
+            f.write("*"+name)
+            f.write('\n')
+            f.write('**'+tag)
+            f.write('\n\n')
     f.close()
+
+def format_dot(name,f):
+    for item in name:
+        if len(item) > 0:
+            ch = re.findall('[\u4e00-\u9fa5]+',item)
+            f.write("*"+''.join(ch))
+            f.write('\n')
+            f.write('**'+item)
+            f.write('\n\n')
 
 def number_string_rule(content):
     data = list(filter(lambda s: re.match(r'\d+\.', s),content.split('\n')))
+    return data
+
+def dot_string_rule(content):
+    data = list(map(lambda name: re.split(r'\d+\.',name),list(filter(lambda s: re.match(r'\d+\.|\-\w+', s),content.split('\n')))))
     return data
 
 def extract_content(file,pages,tag):
@@ -42,7 +63,7 @@ def extract_content(file,pages,tag):
                 extract_page(page,tag)
 
 if __name__ ==  '__main__':
-    file = ''
-    pages = [1,4]
-    tag = ''
+    file = '/Users/nibl/Desktop/书籍/计算机英语/计算机专业英语单词1500.pdf'
+    pages = []
+    tag = '计算机专业英语单词1500.pdf'
     extract_content(file,pages,tag)
